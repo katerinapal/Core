d21b3dad2c0cb655dac9adb5279f31f496aacd60
Katerina Paltoglou <katpaltoglou@gmail.com>
2020-05-15 16:43:26 +0300

    extracting js embed code

--- js/core.js
+++ js/core.js
@@ -712,3 +712,13 @@ window.requestAnimFrame = (function(){
 })();
 
 Core.init();
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-15240703-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
